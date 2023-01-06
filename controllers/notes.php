8a26f6420d36e3c50de44c5b3db348ad65891b1a
felizardochirindja <felizardo.chirindja@gmail.com>
2023-01-06 14:24:13 +0200

    ep 22 - render the notes and the note page

--- controllers/notes.php
+++ controllers/notes.php
@@ -3,9 +3,9 @@
 require './Database.php';
 
 $config = require './config.php';
-
 $db = new Database($config['database']);
-$heading = 'Notes';
+
+$heading = 'My notes';
 
 $notes = $db->query('select * from notes where user_id = 1')->fetchAll();
 
