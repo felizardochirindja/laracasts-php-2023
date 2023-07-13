deb79effc40d2440aedb5e1463779e917ddbcffb
felizardochirindja <felizardo.chirindja@gmail.com>
2023-07-13 22:50:06 +0200

    use a container on notes/index.php

--- controllers/notes/index.php
+++ controllers/notes/index.php
@@ -3,11 +3,7 @@
 use Core\App;
 use Core\Database;
 
-// $config = require basePath('config.php');
-// $db = new Database($config['database']);
-
 $db = App::getContainer()->resolve(Database::class);
-// dumpDie($db);
 
 $notes = $db->query('select * from notes where user_id = 6')->findAll();
 
