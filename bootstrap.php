3d3532b9582a8369821ead7ef723fdb937243cfc
felizardochirindja <felizardo.chirindja@gmail.com>
2023-07-13 23:36:57 +0200

    export app start logic form index to bootstrap file

--- bootstrap.php
+++ bootstrap.php
@@ -2,14 +2,24 @@
 
 use Core\App;
 use Core\Container;
-use Core\Database;
+use Core\Router;
+
+spl_autoload_register(function (string $class) {
+    $class = str_replace('\\', DIRECTORY_SEPARATOR, $class);
+
+    require basePath($class . '.php');
+});
 
 $container = new Container();
 
-$container->bind(Database::class, function() {
-    $config = require basePath('config.php');
-
-    return new Database($config['database']);
-});
+$dependecies = require basePath('config/di.php');
+$dependecies($container);
 
 App::setContainer($container);
+
+$router = new Router();
+$routes = require basePath('routes.php');
+
+if (!$router->route()) {
+    abort();
+}
--- public/index.php
+++ public/index.php
@@ -1,21 +1,5 @@
 <?php
 
-use Core\Router;
-
-$config = require __DIR__ . '/../config.php';
+$config = require __DIR__ . '/../env.php';
 require $config['basePath'] . 'core/functions.php';
-
-spl_autoload_register(function (string $class) {
-    $class = str_replace('\\', DIRECTORY_SEPARATOR, $class);
-
-    require basePath($class . '.php');
-});
-
 require basePath('./bootstrap.php');
-
-$router = new Router();
-$routes = require basePath('routes.php');
-
-if (!$router->route()) {
-    abort();
-}
