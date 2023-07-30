a48555231b00e02a93125e0c587a65dfa87f2c02
felizardochirindja <felizardo.chirindja@gmail.com>
2023-07-30 21:15:44 +0200

    add return type to bind callback

--- config/di.php
+++ config/di.php
@@ -5,7 +5,7 @@ use Core\Database;
 
 return function(Container $container): void
 {
-    $container->bind(Database::class, function() {
+    $container->bind(Database::class, function(): Database {
         $config = require basePath('env.php');
     
         return new Database($config['database']);
