35cd691f75ee8187115a2613874f689eac5d172f
felizardochirindja <felizardo.chirindja@gmail.com>
2023-08-08 12:46:41 +0200

    add logout feature on auth module

--- src/modules/auth/authRoutes.php
+++ src/modules/auth/authRoutes.php
@@ -5,4 +5,5 @@ use Core\Router;
 return function(Router $router): void {
     $router->get('/sign-up', '/auth/controllers/views/signUp');
     $router->post('/store-user', 'auth/controllers/actions/storeUser');
+    $router->post('/logout', 'auth/controllers/actions/logout');
 };
--- src/modules/shared/views/partials/nav.php
+++ src/modules/shared/views/partials/nav.php
@@ -6,12 +6,21 @@
                     <div class="flex items-baseline space-x-4">
                         <a href="/" class="<?= requestUrlIs('/') ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white' ?> px-3 py-2 rounded-md text-sm font-medium"><?= requestUrlIs("/") ? "HOME" : "Home" ?></a>
                         <a href="/about" class="<?= requestUrlIs('/about') ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white' ?> px-3 py-2 rounded-md text-sm font-medium"><?= requestUrlIs("/about") ? "ABOUT" : "About" ?></a>
-                        <a href="/notes" class="<?= requestUrlIs('/notes') ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white' ?> px-3 py-2 rounded-md text-sm font-medium"><?= requestUrlIs("/notes") ? "NOTES" : "Notes" ?></a>
+                        
+                        <?php if ($_SESSION['email'] ?? false): ?>
+                            <a href="/notes" class="<?= requestUrlIs('/notes') ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white' ?> px-3 py-2 rounded-md text-sm font-medium"><?= requestUrlIs("/notes") ? "NOTES" : "Notes" ?></a>
+                        <?php endif; ?>
+                        
                         <a href="/contact" class="<?= requestUrlIs('/contact') ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white' ?> px-3 py-2 rounded-md text-sm font-medium"><?= requestUrlIs("/contact") ? "CONTACT" : "Contact" ?></a>
                         
-                        <?php if (!array_key_exists('email', $_SESSION)): ?>
+                        <!-- auth buttons -->
+                        <?php if (!($_SESSION['email'] ?? false) ): ?>
                             <a href="/sign-up" class="<?= requestUrlIs('/sign-up') ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white' ?> px-3 py-2 rounded-md text-sm font-medium"><?= requestUrlIs("/sign-up") ? "SIGN-UP" : "Sign-up" ?></a>
-                        <?php endif ?>
+                        <?php else: ?>
+                            <form action="/logout" method="post">
+                                <input type="submit" value="logout" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
+                            </form>
+                        <?php endif; ?>
                     </div>
                 </div>
             </div>
