fcc5f034e794640c00c37e8941453b022c2d30da
felizardochirindja <felizardo.chirindja@gmail.com>
2023-08-11 17:12:02 +0200

    bind AuthService dependency

--- config/di.php
+++ config/di.php
@@ -2,10 +2,17 @@
 
 use Core\Container;
 use Core\Database;
+use Modules\Auth\AuthService;
 
 return function(Container $container): void {
     $container->bind(Database::class, function() use ($container): Database {
         $config = $container->resolve('settings')['database'];
         return new Database($config);
+    });
+    
+    $container->bind(AuthService::class, function() use ($container) : AuthService {
+        /** @var Database $db */
+        $db = $container->resolve(Database::class);
+        return new AuthService($db);
     });
 };
