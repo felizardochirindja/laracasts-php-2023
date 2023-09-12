88fb7c48448cb2213aa7a7c309e5978e88e97e88
felizardochirindja <felizardo.chirindja@gmail.com>
2023-09-12 17:45:14 +0200

    ref: remove all the object oriented stuff on engine.php

--- src/engine.php
+++ src/engine.php
@@ -1,38 +1,23 @@
 <?php
 
-use Core\App;
-use Core\Container;
-use Core\Router;
-
 spl_autoload_register(function(string $fullClassName): void {
     $fullClassName = str_replace('\\', DIRECTORY_SEPARATOR, $fullClassName);
     require fileFromRoot($fullClassName);
 });
+    
+$routes = [];
 
-$container = new Container();
-
-$settings = require fileFromRoot('config/settings', '/');
-$settings($container);
-
-$dependecies = require fileFromRoot('config/di', '/');
-$dependecies($container);
-
-App::setContainer($container);
-
-$router = new Router();
-
-// page routes
-$routes = require fileFromRoot('modules/pages/routes');
-$routes($router);
+// pages routes
+$routes[] = require fileFromRoot('modules/pages/routes');
 
 // note routes
-$routes = require fileFromRoot('modules/note/noteRoutes');
-$routes($router);
+$routes[] = require fileFromRoot('modules/note/noteRoutes');
 
 // auth routes
-$routes = require fileFromRoot('modules/auth/authRoutes');
-$routes($router);
 
-if (!$router->navigate()) {
+// $routes = $route
+$routeFound = require fileFromRoot('core/router');
+
+if (!$routeFound) {
     breakPage();
 }
